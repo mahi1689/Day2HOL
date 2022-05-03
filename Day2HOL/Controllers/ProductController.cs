fba2d0323a27570b3b4cd68730f79369109c6f34
Mahesh1997 <Mahesh1997@MaheshMCapgemin>
2022-05-03 21:47:54 +0530

    Add project files.

---
+++ Day2HOL/Controllers/ProductController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Day2HOL.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            Models.Product p = new Models.Product();
+            return View(p.GetProducts());
+        }
+    }
+}
